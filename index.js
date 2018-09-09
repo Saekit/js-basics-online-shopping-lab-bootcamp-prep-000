00b463797f096400cddf5a9b21163df56d4e9204
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-09 18:05:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,64 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  var shoppingCart = new Object({ itemName: item, itemPrice: price });
+  cart.push(shoppingCart);
+  return (`${shoppingCart.itemName} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+   if (cart.length === 0) {
+       return ("Your shopping cart is empty.");
+   } else {
+       var itemsAndPrice = [];
+       for(var i = 0; i < cart.length; i++) {
+          var cartItems = Object.values(cart[i]).join(' at $');
+          itemsAndPrice.push(cartItems);
+       }
+     var final = itemsAndPrice.splice(itemsAndPrice.length - 1, 0, "and ");
+        return (`In your cart, you have ${[itemsAndPrice.join(', ')]}.`);
+   }
 }
 
 function total() {
-  // write your code here
+  var price1 = Object.values(cart[0]).pop();
+  var price2 = Object.values(cart[1]).pop();
+  var price3 = Object.values(cart[2]).pop();  
+  var total = price1 + price2 + price3;
+  return(`$${total}`);
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    var firstItem = Object.values(cart[i]).slice(0, 1);
+    var secondItem = Object.values(cart[i + 1]).slice(0, 1);
+    var thirdItem = Object.values(cart[i + 2]).slice(0, 1);
+ 
+    if (item === firstItem.toString()) {
+    cart.splice(0, 1);
+    return cart;
+    }
+    else if (item === secondItem.toString()) {
+    cart.splice(1, 1);
+    return cart;
+    }
+    else if (item === thirdItem.toString()) {
+    cart.splice(2, 1);
+    return cart;
+    }
+    else if (item){
+      return ("That item is not in your cart.");
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+ if (!cardNumber) {
+    return ("Sorry, we don't have a credit card on file for you.");
+  }
+  else {
+    cart = [];
+    return (`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+  }
 }
