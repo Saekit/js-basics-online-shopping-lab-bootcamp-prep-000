eef588f3b122a712ecfac98583f7bd08c19dbb9a
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-09 20:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ function placeOrder(cardNumber) {
  if (!cardNumber) {
     return ("Sorry, we don't have a credit card on file for you.");
   } else {
-    var total = total();
+    var final = (`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
- return (`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
+  return final;
   }
 }
