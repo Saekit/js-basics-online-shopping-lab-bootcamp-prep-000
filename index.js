c17c8a91ea1016c77546349617be60ada77d5f03
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-09 18:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,10 +44,8 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     }
-    else if (!item){
-      return ("That item is not in your cart.");
-    }
   }
+  return ("That item is not in your cart.");
 }
 
 function placeOrder(cardNumber) {
